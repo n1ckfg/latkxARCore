d34d102d095a769e71df749646916f4bf2309138
Nick Fox-Gieg <nick@fox-gieg.com>
2020-01-12 13:48:10 -0500

    Update ARCoreButtons.cs

--- Scripts/ARCoreButtons.cs
+++ Scripts/ARCoreButtons.cs
@@ -46,14 +46,14 @@ public class ARCoreButtons : MonoBehaviour
             }
 
             // 1-2.
-            Rect undoButton = new Rect(BUTTON_GAP_X, Screen.height - (2 * (BUTTON_SIZE_Y - BUTTON_GAP_X)), BUTTON_SIZE_X, BUTTON_SIZE_Y);
+            Rect undoButton = new Rect(BUTTON_GAP_X, Screen.height - (3 * (BUTTON_SIZE_Y - BUTTON_GAP_X)), BUTTON_SIZE_X, BUTTON_SIZE_Y);
             //isOn = lightningArtist.showOnionSkin ? "Off" : "On";
             if (GUI.Button(undoButton, FONT_SIZE + "Undo" + "</size>")) {
                 lightningArtist.inputEraseLastStroke();
             }
 
             // 1-3.
-            Rect colorButton = new Rect(BUTTON_GAP_X, Screen.height - (3 * (BUTTON_SIZE_Y - BUTTON_GAP_X)), BUTTON_SIZE_X, BUTTON_SIZE_Y);
+            Rect colorButton = new Rect(BUTTON_GAP_X, Screen.height - (4 * (BUTTON_SIZE_Y - BUTTON_GAP_X)), BUTTON_SIZE_X, BUTTON_SIZE_Y);
             isOn = showHideGeneric.target[0].activeSelf ? "ON" : "OFF";
             if (GUI.Button(colorButton, FONT_SIZE + "Palette " + isOn + "</size>")) {
                 if (showHideGeneric.target[0].activeSelf) {
@@ -64,13 +64,14 @@ public class ARCoreButtons : MonoBehaviour
             }
 
             // 1-4.
-            Rect onionButton = new Rect(BUTTON_GAP_X, Screen.height - (4 * (BUTTON_SIZE_Y - BUTTON_GAP_X)), BUTTON_SIZE_X, BUTTON_SIZE_Y);
+            Rect onionButton = new Rect(BUTTON_GAP_X, Screen.height - (5 * (BUTTON_SIZE_Y - BUTTON_GAP_X)), BUTTON_SIZE_X, BUTTON_SIZE_Y);
             isOn = lightningArtist.showOnionSkin ? "ON" : "OFF";
             if (GUI.Button(onionButton, FONT_SIZE + "Onion Skin " + isOn + "</size>")) {
                 lightningArtist.inputOnionSkin();
             }
 
             // 1-5.
+            /*
             Rect rotoButton = new Rect(BUTTON_GAP_X, Screen.height - (5 * (BUTTON_SIZE_Y - BUTTON_GAP_X)), BUTTON_SIZE_X, BUTTON_SIZE_Y);
             isOn = brushInputTango.rotoEnabled ? "ON" : "OFF";
             if (GUI.Button(rotoButton, FONT_SIZE + "Roto " + isOn + "</size>")) {
@@ -83,50 +84,51 @@ public class ARCoreButtons : MonoBehaviour
             if (GUI.Button(drawButton, FONT_SIZE + "Draw " + isOn + "</size>")) {
                 brushInputTango.drawEnabled = !brushInputTango.drawEnabled;
             }
+            */
 
             // 1-7.
-            Rect copyFrameButton = new Rect(BUTTON_GAP_X, Screen.height - (7 * (BUTTON_SIZE_Y - BUTTON_GAP_X)), BUTTON_SIZE_X, BUTTON_SIZE_Y);
+            Rect copyFrameButton = new Rect(BUTTON_GAP_X, Screen.height - (6 * (BUTTON_SIZE_Y - BUTTON_GAP_X)), BUTTON_SIZE_X, BUTTON_SIZE_Y);
             //isOn = m_arCameraPostProcess.enabled ? "Off" : "On";
             if (GUI.Button(copyFrameButton, FONT_SIZE + "Copy Frame " + "</size>")) {
                 lightningArtist.inputNewFrameAndCopy();
             }
 
             // 1-8.
-            Rect newFrameButton = new Rect(BUTTON_GAP_X, Screen.height - (8 * (BUTTON_SIZE_Y - BUTTON_GAP_X)), BUTTON_SIZE_X, BUTTON_SIZE_Y);
+            Rect newFrameButton = new Rect(BUTTON_GAP_X, Screen.height - (7 * (BUTTON_SIZE_Y - BUTTON_GAP_X)), BUTTON_SIZE_X, BUTTON_SIZE_Y);
             //isOn = m_arCameraPostProcess.enabled ? "Off" : "On";
             if (GUI.Button(newFrameButton, FONT_SIZE + "New Frame " + "</size>")) {
                 lightningArtist.inputNewFrame();
             }
 
             // 1-9.
-            Rect playButton = new Rect(BUTTON_GAP_X, Screen.height - (9 * (BUTTON_SIZE_Y - BUTTON_GAP_X)), BUTTON_SIZE_X, BUTTON_SIZE_Y);
+            Rect playButton = new Rect(BUTTON_GAP_X, Screen.height - (8 * (BUTTON_SIZE_Y - BUTTON_GAP_X)), BUTTON_SIZE_X, BUTTON_SIZE_Y);
             isOn = lightningArtist.isPlaying ? "Stop" : "Play";
             if (GUI.Button(playButton, FONT_SIZE + isOn + "</size>")) {
                 lightningArtist.inputPlay();
             }
 
             // 1-10.
-            Rect rewButton = new Rect(BUTTON_GAP_X, Screen.height - (10 * (BUTTON_SIZE_Y - BUTTON_GAP_X)), BUTTON_SIZE_X / 2f, BUTTON_SIZE_Y);
+            Rect rewButton = new Rect(BUTTON_GAP_X, Screen.height - (9 * (BUTTON_SIZE_Y - BUTTON_GAP_X)), BUTTON_SIZE_X / 2f, BUTTON_SIZE_Y);
             //isOn = m_arCameraPostProcess.enabled ? "Off" : "On";
             if (GUI.Button(rewButton, FONT_SIZE + "<|" + "</size>")) {
                 lightningArtist.inputFrameBack();
             }
 
-            Rect ffButton = new Rect(BUTTON_GAP_X + (BUTTON_SIZE_X / 2f), Screen.height - (10 * (BUTTON_SIZE_Y - BUTTON_GAP_X)), BUTTON_SIZE_X / 2f, BUTTON_SIZE_Y);
+            Rect ffButton = new Rect(BUTTON_GAP_X + (BUTTON_SIZE_X / 2f), Screen.height - (9 * (BUTTON_SIZE_Y - BUTTON_GAP_X)), BUTTON_SIZE_X / 2f, BUTTON_SIZE_Y);
             //isOn = m_arCameraPostProcess.enabled ? "Off" : "On";
             if (GUI.Button(ffButton, FONT_SIZE + "|>" + "</size>")) {
                 lightningArtist.inputFrameForward();
             }
         } else if (menuCounter == 2) {
             // 2-10.
-            Rect layerChangeButton = new Rect(BUTTON_GAP_X, Screen.height - (10 * (BUTTON_SIZE_Y - BUTTON_GAP_X)), BUTTON_SIZE_X, BUTTON_SIZE_Y);
+            Rect layerChangeButton = new Rect(BUTTON_GAP_X, Screen.height - (9 * (BUTTON_SIZE_Y - BUTTON_GAP_X)), BUTTON_SIZE_X, BUTTON_SIZE_Y);
             //isOn = lightningArtist.showOnionSkin ? "Off" : "On";
             if (GUI.Button(layerChangeButton, FONT_SIZE + "Next Layer" + "</size>")) {
                 lightningArtist.inputNextLayer();
             }
 
             // 2-9.
-            Rect newLayerButton = new Rect(BUTTON_GAP_X, Screen.height - (9 * (BUTTON_SIZE_Y - BUTTON_GAP_X)), BUTTON_SIZE_X, BUTTON_SIZE_Y);
+            Rect newLayerButton = new Rect(BUTTON_GAP_X, Screen.height - (8 * (BUTTON_SIZE_Y - BUTTON_GAP_X)), BUTTON_SIZE_X, BUTTON_SIZE_Y);
             //isOn = lightningArtist.showOnionSkin ? "Off" : "On";
             if (GUI.Button(newLayerButton, FONT_SIZE + "New Layer" + "</size>")) {
                 lightningArtist.inputNewLayer();
@@ -141,7 +143,7 @@ public class ARCoreButtons : MonoBehaviour
         }
 
         // 1-11.
-        Rect menuButton = new Rect(BUTTON_GAP_X, Screen.height - (11 * (BUTTON_SIZE_Y - BUTTON_GAP_X)), BUTTON_SIZE_X, BUTTON_SIZE_Y);
+        Rect menuButton = new Rect(BUTTON_GAP_X, Screen.height - (10 * (BUTTON_SIZE_Y - BUTTON_GAP_X)), BUTTON_SIZE_X, BUTTON_SIZE_Y);
         //isOn = m_arCameraPostProcess.enabled ? "Off" : "On";
         if (GUI.Button(menuButton, FONT_SIZE + "MENU " + menuCounter + "</size>")) {
             menuCounter++;
